a89aff4f63f23ecb4db214c595cb8d86f0800643
pujadas02 <pd4759150@gmail.com>
2024-12-16 11:46:54 +0530

    Update rev.tf

--- rev.tf
+++ rev.tf
@@ -2,32 +2,4 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_security_group" "example" {
-  name        = "secure-example-sg"
-  description = "Allow only HTTP and HTTPS traffic"
-
-  # Allow HTTP traffic
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # Allow HTTPS traffic
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # Allow all outbound traffic (default for most use cases)
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
 

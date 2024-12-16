cf1c35ad061015113d53e1bca360aa58262afd8f
Das <puja.i.das@accenture.com>
2024-12-16 17:51:50 +0530

    e

--- rev.tf
+++ rev.tf
@@ -10,4 +10,12 @@ resource "azurerm_resource_group" "example" {
     app = "myapp"
   }
 }
+resource "azurerm_network_security_group" "example" {
+  name                = "example-nsg"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
 
+  tags = {
+    a = "myApp"  # Add the "app" tag here as well
+  }
+}

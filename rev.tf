7961fac1fa9b5df12d13d97f527a8736daeba966
Das <puja.i.das@accenture.com>
2024-12-17 17:06:02 +0530

    not allowed resource type01

--- rev.tf
+++ rev.tf
@@ -19,62 +19,34 @@
 #     a = "myApp" 
 #   }
 # }
-# # resource "azurerm_monitor_action_group" "example" {
-# #   name                = "example-action-group"
-# #   resource_group_name = "example-resource-group"
-# #   short_name          = "exampleag"
+# resource "azurerm_monitor_action_group" "example" {
+#   name                = "example-action-group"
+#   resource_group_name = "example-resource-group"
+#   short_name          = "exampleag"
   
   
-# #   tags = {
-# #     app = "myApp" 
-# #   }
-# # }
+#   tags = {
+#     app = "myApp" 
+#   }
+# }
+
+
+
 
 provider "azurerm" {
   features {}
 }
 
 # Create Resource Group
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
+resource "azurerm_resource_group" "vwan_rg" {
+  name     = "vwan-deployment-rg"
   location = "East US"
 }
 
-# Create a Virtual Network
-resource "azurerm_virtual_network" "example" {
-  name                = "example-vnet"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  address_space       = ["10.0.0.0/16"]
+# Create Virtual WAN
+resource "azurerm_virtual_wan" "vwan" {
+  name                = "example-vwan"
+  location            = azurerm_resource_group.vwan_rg.location
+  resource_group_name = azurerm_resource_group.vwan_rg.name
 }
 
-# Create a Subnet
-resource "azurerm_subnet" "example" {
-  name                 = "example-subnet"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
-  address_prefixes     = ["10.0.1.0/24"]
-}
-
-# Create a Public IP
-resource "azurerm_public_ip" "example" {
-  name                = "example-public-ip"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  allocation_method   = "Dynamic"
-  
-}
-
-# Create Network Interface with Mistakenly Attached Public IP
-resource "azurerm_network_interface" "example" {
-  name                = "example-nic"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-
-  ip_configuration {
-    name                          = "example-ip-config"
-    subnet_id                     = azurerm_subnet.example.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.example.id 
-  }
-}

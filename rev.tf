30fbe89d0b137cf7c17295c477db387f29c3282d
Das <puja.i.das@accenture.com>
2024-12-23 16:55:02 +0530

    sku check 1

--- rev.tf
+++ rev.tf
@@ -9,69 +9,107 @@
 #   resource_group_name = "example-rg"
 # }
 
+
+
+# this is one
+# provider "azurerm" {
+#   features {}
+# }
+
+# # Resource Group
+# resource "azurerm_resource_group" "example" {
+#   name     = "example-resources"
+#   location = "East US"
+# }
+
+# # Managed Disk
+# resource "azurerm_managed_disk" "example_disk" {
+#   name                 = "example-managed-disk"
+#   resource_group_name  = azurerm_resource_group.example.name
+#   location             = azurerm_resource_group.example.location
+  
+#   storage_account_type = "Standard_LRS"
+#   create_option        = "Empty"
+# }
+
+# # Virtual Network
+# resource "azurerm_virtual_network" "example_vnet" {
+#   name                = "example-vnet"
+#   location            = azurerm_resource_group.example.location
+#   resource_group_name = azurerm_resource_group.example.name
+#   address_space       = ["10.0.0.0/16"]
+# }
+
+# # Network Interface
+# resource "azurerm_network_interface" "example_nic" {
+#   name                = "example-nic"
+#   location            = azurerm_resource_group.example.location
+#   resource_group_name = azurerm_resource_group.example.name
+
+#   ip_configuration {
+#     name                          = "example-ipconfig"
+#     subnet_id                     = azurerm_virtual_network.example_vnet.subnet[0].id
+#     private_ip_address_allocation = "Dynamic"
+#   }
+# }
+
+# # Storage Account
+# resource "azurerm_storage_account" "example_storage" {
+#   name                     = "examplestorageacct"
+#   resource_group_name      = azurerm_resource_group.example.name
+#   location                 = azurerm_resource_group.example.location
+#   account_tier              = "Standard"
+#   account_replication_type = "LRS"
+# }
+
+# # SQL Server
+# resource "azurerm_sql_server" "example_sql_server" {
+#   name                         = "example-sql-server"
+#   resource_group_name          = azurerm_resource_group.example.name
+#   location                     = azurerm_resource_group.example.location
+#   version                      = "12.0"
+#   administrator_login          = "sqladmin"
+#   administrator_login_password = "password1234!"
+
+#   tags = {
+#     environment = "production"
+#   }
+# }
+
+
+
+#this is 2nd
+
 provider "azurerm" {
   features {}
 }
 
-# Resource Group
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "East US"
+resource "azurerm_public_ip" "example" {
+  name                = "myBasicPublicIP"
+  location            = "East US"
+  resource_group_name = "myResourceGroup"
+  allocation_method   = "Dynamic"  # Or "Static" for a fixed IP
+  sku                  = "Basic"
 }
 
-# Managed Disk
-resource "azurerm_managed_disk" "example_disk" {
-  name                 = "example-managed-disk"
-  resource_group_name  = azurerm_resource_group.example.name
-  location             = azurerm_resource_group.example.location
-  
-  storage_account_type = "Standard_LRS"
-  create_option        = "Empty"
-}
 
-# Virtual Network
-resource "azurerm_virtual_network" "example_vnet" {
-  name                = "example-vnet"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  address_space       = ["10.0.0.0/16"]
-}
 
-# Network Interface
-resource "azurerm_network_interface" "example_nic" {
-  name                = "example-nic"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
 
-  ip_configuration {
-    name                          = "example-ipconfig"
-    subnet_id                     = azurerm_virtual_network.example_vnet.subnet[0].id
-    private_ip_address_allocation = "Dynamic"
-  }
-}
 
-# Storage Account
-resource "azurerm_storage_account" "example_storage" {
-  name                     = "examplestorageacct"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
-  account_tier              = "Standard"
-  account_replication_type = "LRS"
-}
 
-# SQL Server
-resource "azurerm_sql_server" "example_sql_server" {
-  name                         = "example-sql-server"
-  resource_group_name          = azurerm_resource_group.example.name
-  location                     = azurerm_resource_group.example.location
-  version                      = "12.0"
-  administrator_login          = "sqladmin"
-  administrator_login_password = "password1234!"
 
-  tags = {
-    environment = "production"
-  }
-}
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 

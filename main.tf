eeb15c54387862272073df11538d456e447c6525
pujadas02 <pd4759150@gmail.com>
2025-03-17 15:26:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,13 @@
-resource "azurerm_resource_group" "rg01" {
-  name     = var.name
-  location = var.location
-}
-  
+resource "azurerm_monitor_diagnostic_setting" "example" {
+  name                       = "example-diagnostic-setting"
+  target_resource_id         = azurerm_virtual_machine.example.id
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.example.id
 
+  enabled_log {
+    category = "AuditLogs"
+  }
+
+  metric {
+    category = "AllMetrics"
+  }
+}

dbdddf4d92a4d69b312107594a2a3db79844ff81
pujadas02 <pd4759150@gmail.com>
2025-03-18 18:23:17 +0530

    Update workflow.py

--- custom/workflow.py
+++ custom/workflow.py
@@ -1,57 +1,91 @@
 from __future__ import annotations
+
 from typing import Any
 from checkov.common.models.enums import CheckResult, CheckCategories
 from checkov.terraform.checks.resource.base_resource_check import BaseResourceCheck
 
-class EnsureSnapshotLifetimeTagExistsCheck(BaseResourceCheck):
+class EnsureResourcesLocation(BaseResourceCheck):
     def __init__(self) -> None:
-        name = "Ensure business_criticality tag exists. Valid Values are [A+,a+,A,a,B,b,C,c,Z,z,Tier 0,Tier0,T0,tier 0,tier0,t0,Tier 1,Tier1,T1,tier 1,tier1,t1,N/A,NA]"
-        id =  "CCOE_AZ2_16"
+        name = "Ensure resources are deployed only at allowed locations.(eastus2,centralus,westeurope,northeurope,southeastasia,eastasia,global,uksouth)"
+        id = "CCOE_AZ2_16"
         supported_resources = ['azurerm_*']
         categories = [CheckCategories.BACKUP_AND_RECOVERY]
         super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
 
     def scan_resource_conf(self, conf: dict[str, list[Any]]) -> CheckResult:
-        print("Scanning resource:", conf)  # Debugging line to check if it's being executed
         excluded_resource_types = [
-            "azurerm_virtual_machine_extension",
-            "azurerm_monitor_smart_detector_alert_rule",
-            "azurerm_monitor_action_group",
-            "azurerm_security_center_automation",
-            "azurerm_network_watcher",
-            "azurerm_network_connection_monitor",
-            "azurerm_network_watcher_flow_log",
-            "azurerm_virtual_hub",
-            "azurerm_management_group_template_deployment",
-            "azurerm_resource_group_template_deployment",
-            "azurerm_subscription_template_deployment",
-            "azurerm_tenant_template_deployment",
-            "azurerm_network_security_group",
-            "azurerm_network_security_rule",
-            "azurerm_automation_runbook",
-            "azurerm_api_connection",
-            "azurerm_role_assignment",
-            # "azurerm_resource_group"
+            "azurerm_aadb2c_directory",
+            "azurerm_resource_group"
         ]
         resource_type = conf.get("__address__")
         if resource_type:
             for excluded_type in excluded_resource_types:
                 if excluded_type in resource_type:
                     return CheckResult.SKIPPED
-            tags = conf.get("tags")
-            if tags and isinstance(tags, list):
-                tags = tags[0]
-                if tags and isinstance(tags, dict):
-                    business_criticality = tags.get("business_criticality")
-                    if business_criticality is not None and business_criticality in ["A+", "a+", "A", "a", "B", "b", "C", "c", "Z", "z", "Tier 0", "Tier0", "T0, ", "tier 0", "tier0", "t0", "Tier 1", "Tier1", "T1", "tier 1", "tier1", "t1", "N/A", "NA"]:
-                        return CheckResult.PASSED 
-                    else:
-                        return CheckResult.FAILED
-            else:
-                if not tags:
+            location = conf.get("location")
+            if location and isinstance(location, list):
+                location = location[0]
+                if location is not None and location in ["eastus2", "centralus", "westeurope", "northeurope", "southeastasia", "eastasia", "global", "uksouth", "EastUS2", "CentralUS", "NorthEurope", "WestEurope", "SoutheastAsia", "EastAsia"]:
+                    return CheckResult.PASSED
+                else:
                     return CheckResult.FAILED
 
-check = EnsureSnapshotLifetimeTagExistsCheck()
+check = EnsureResourcesLocation()
+
+# from __future__ import annotations
+# from typing import Any
+# from checkov.common.models.enums import CheckResult, CheckCategories
+# from checkov.terraform.checks.resource.base_resource_check import BaseResourceCheck
+
+# class EnsureSnapshotLifetimeTagExistsCheck(BaseResourceCheck):
+#     def __init__(self) -> None:
+#         name = "Ensure business_criticality tag exists. Valid Values are [A+,a+,A,a,B,b,C,c,Z,z,Tier 0,Tier0,T0,tier 0,tier0,t0,Tier 1,Tier1,T1,tier 1,tier1,t1,N/A,NA]"
+#         id =  "CCOE_AZ2_16"
+#         supported_resources = ['azurerm_*']
+#         categories = [CheckCategories.BACKUP_AND_RECOVERY]
+#         super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
+
+#     def scan_resource_conf(self, conf: dict[str, list[Any]]) -> CheckResult:
+#         print("Scanning resource:", conf)  # Debugging line to check if it's being executed
+#         excluded_resource_types = [
+#             "azurerm_virtual_machine_extension",
+#             "azurerm_monitor_smart_detector_alert_rule",
+#             "azurerm_monitor_action_group",
+#             "azurerm_security_center_automation",
+#             "azurerm_network_watcher",
+#             "azurerm_network_connection_monitor",
+#             "azurerm_network_watcher_flow_log",
+#             "azurerm_virtual_hub",
+#             "azurerm_management_group_template_deployment",
+#             "azurerm_resource_group_template_deployment",
+#             "azurerm_subscription_template_deployment",
+#             "azurerm_tenant_template_deployment",
+#             "azurerm_network_security_group",
+#             "azurerm_network_security_rule",
+#             "azurerm_automation_runbook",
+#             "azurerm_api_connection",
+#             "azurerm_role_assignment",
+#             # "azurerm_resource_group"
+#         ]
+#         resource_type = conf.get("__address__")
+#         if resource_type:
+#             for excluded_type in excluded_resource_types:
+#                 if excluded_type in resource_type:
+#                     return CheckResult.SKIPPED
+#             tags = conf.get("tags")
+#             if tags and isinstance(tags, list):
+#                 tags = tags[0]
+#                 if tags and isinstance(tags, dict):
+#                     business_criticality = tags.get("business_criticality")
+#                     if business_criticality is not None and business_criticality in ["A+", "a+", "A", "a", "B", "b", "C", "c", "Z", "z", "Tier 0", "Tier0", "T0, ", "tier 0", "tier0", "t0", "Tier 1", "Tier1", "T1", "tier 1", "tier1", "t1", "N/A", "NA"]:
+#                         return CheckResult.PASSED 
+#                     else:
+#                         return CheckResult.FAILED
+#             else:
+#                 if not tags:
+#                     return CheckResult.FAILED
+
+# check = EnsureSnapshotLifetimeTagExistsCheck()
 
 
 

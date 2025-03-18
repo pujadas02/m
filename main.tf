65feeb66db94da9d1143ece162dfa58c12dbb275
pujadas02 <pd4759150@gmail.com>
2025-03-18 19:28:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,16 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "example-rg"
-  location = "East US"
+resource "azurerm_monitor_diagnostic_setting" "logic" {
+  for_each                   = local.logicapploop
+  name                       = "OperationLogs"
+  target_resource_id         = azurerm_logic_app_standard.logic[each.key].id
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.log01.id
+  enabled_log {
+    category = "WorkflowRuntime"
+  }
+  metric {
+    category = "AllMetrics"
+  }
 }
+

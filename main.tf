2eba2d43df7bb70f2ae7409993dd153389874a82
pujadas02 <pd4759150@gmail.com>
2025-03-25 12:42:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "azurerm_monitor_diagnostic_setting" "logic" {
   for_each                   = local.logicapploop
   name                       = "OperationLogs"
   target_resource_id         = azurerm_logic_app_standard.logic[each.key].id
-  log_analytics_workspace_id = azurerm_log_analytics_workspace.log01.id
+ 
   enabled_log {
     category = "WorkflowRuntime"
   }

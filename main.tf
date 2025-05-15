7edb34b18e4ae75d4aeb03f4b9a9eca383548097
pujadas02 <pd4759150@gmail.com>
2025-05-15 16:07:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,8 +9,7 @@ resource "azurerm_resource_group" "primary" {
 
 resource "azurerm_monitor_diagnostic_setting" "logic20" {
   name                       = "OperationLogs"
-  target_resource_id         = azurerm_logic_app_standard
-  log_analytics_workspace_id = azurerm_log_analytics_workspace
+ 
 
   enabled_log {
     category = "WorkflowRuntime"

7398cdea0950fac0e84cc8045bf7f321819fbb27
pujadas02 <pd4759150@gmail.com>
2025-05-15 16:03:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,4 +7,16 @@ resource "azurerm_resource_group" "primary" {
   location = "Eastuss"
 }
 
+resource "azurerm_monitor_diagnostic_setting" "logic20" {
+  name                       = "OperationLogs"
+  target_resource_id         = azurerm_logic_app_standard.logic_apps["logic-marsis-np-eu2-20"].id
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.log01.id
 
+  enabled_log {
+    category = "WorkflowRuntime"
+  }
+
+  metric {
+    category = "AllMetrics"
+  }
+}

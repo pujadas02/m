ddbaa3a205f969b8ecef8a3f0344d8c9c741bbf7
pujadas02 <pd4759150@gmail.com>
2025-05-19 15:38:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,21 +1,26 @@
 provider "azurerm" {
   features {}
 }
-
 resource "azurerm_resource_group" "primary" {
   name     = "hi"
   location = "Eastuss"
 }
 
+
 resource "azurerm_monitor_diagnostic_setting" "logic20" {
   name                       = "OperationLogs"
- 
-
   enabled_log {
     category = "WorkflowRuntime"
   }
-
   metric {
     category = "AllMetrics"
+  }
+}
+
+
+resource "azurerm_dns_zone" "example-public" {
+  name                = "mydomain.com"
+  tags = {
+    business_criticality = "High"
   }
 }

ca345c82fb39460470872bafb279310ae22bf90a
pujadas02 <pd4759150@gmail.com>
2025-05-20 13:31:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -26,30 +26,8 @@ resource "azurerm_dns_zone" "example-public" {
 }
 resource "azurerm_resource_group" "rg_01" {
   name                = "mydomain.com"
-  tags = {
-    common_tags = {
-      app                  = "Litmus IoT Edge"
-      ppm_id_owner         = "Reeves, Lee"
-      ppm_io_cc            = "81007982"
-      environment          = "production"
-      data_classification  = "confidential"
-      business_criticality = "c"
-      app_owner_group      = "PNT-DATAINGESTION-GLOBAL"
-      expert_centre        = "PNT-DATAINGESTION-GLOBAL"
-      snapshotlifetime     = "1"
-    }
-
-    cvlt_backup = {
-      non_iaas = {
-        cvlt_backup = "cvlt_no_backup"
-      }
-
-      app_server = {
-        cvlt_backup        = "cvlt_vsa_file"
-        ppm_billing_item   = "PET- MANUFACTURING DATA MANAGEMENT DESIGN AND PILOT"
-        ppm_funding_source = "IO/CC"
-      }
-    }
-  }
+  tags     = merge(local.tags.common_tags, local.tags.cvlt_backup.non_iaas)
 }
+  
+
   

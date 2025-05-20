6ba6e1e29ecc24526dd34a352714fd33c8f50b03
pujadas02 <pd4759150@gmail.com>
2025-05-20 12:07:14 +0530

    Update checks.py

--- custom/checks.py
+++ custom/checks.py
@@ -13,27 +13,6 @@ class EnsureSnapshotLifetimeTagExistsCheck(BaseResourceCheck):
         super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
             
         self.docs_url = "https://raw.githubusercontent.com/hashicorp/terraform-provider-azurerm/main/website/docs/r/"
-        self.excluded_resources = [
-            "azurerm_virtual_machine_extension",
-            "azurerm_monitor_smart_detector_alert_rule",
-            "azurerm_monitor_action_group",
-            "azurerm_security_center_automation",
-            "azurerm_network_watcher",
-            "azurerm_network_connection_monitor",
-            "azurerm_network_watcher_flow_log",
-            "azurerm_virtual_hub",
-            "azurerm_management_group_template_deployment",
-            "azurerm_resource_group_template_deployment",
-            "azurerm_subscription_template_deployment",
-            "azurerm_tenant_template_deployment",
-            "azurerm_network_security_group",
-            "azurerm_network_security_rule",
-            "azurerm_automation_runbook",
-            "azurerm_api_connection",
-            "azurerm_role_assignment",
-            "azurerm_resource_group",
-            "azurerm_monitor_diagnostic_setting"
-        ]
     def has_tags_support(self, resource_type):
         try:
             response = requests.get(f"{self.docs_url}{resource_type}.html.markdown", timeout=3)
@@ -44,10 +23,9 @@ class EnsureSnapshotLifetimeTagExistsCheck(BaseResourceCheck):
     def scan_resource_conf(self, conf):
         if not (address := conf.get("__address__", "")):
             return CheckResult.SKIPPED
-        full_resource_type = address.split(".")[0]
-        if any(excluded in full_resource_type for excluded in self.excluded_resources):
-            return CheckResult.SKIPPED
-        resource_type = full_resource_type[8:] 
+            
+        resource_type = address.split(".")[0][8:]
+        
         if not self.has_tags_support(resource_type):
             return CheckResult.SKIPPED    
                 

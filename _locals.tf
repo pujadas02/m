9b613f79de33f63e74feade41901e4c64fbee3ad
pujadas02 <pd4759150@gmail.com>
2025-05-20 16:53:23 +0530

    Update _locals.tf

--- _locals.tf
+++ _locals.tf
@@ -5,7 +5,7 @@ locals {
     app                  = "BigID"                   # App name according to CMDB
     ppm_id_owner         = "Castro, Dominic"         # Cost Center Owner 
     ppm_io_cc            = "11633521"                # Cost Center
-    environment          = "dev"                     # Must be one of these: "production","legacy prod","disaster recovery","qa","QA","dev","Dev","test","Test","sandbox","Sandbox","N/A","NA"
+    environment          = "production"                     # Must be one of these: "production","legacy prod","disaster recovery","qa","QA","dev","Dev","test","Test","sandbox","Sandbox","N/A","NA"
     data_classification  = "confidential"            # Must be one of these: "highly confidential","Highly Confidential","confidential","Confidential","internal use only","internal","nonconfidential","Nonconfidential","non confidential","Non Confidential","N/A","NA" 
     business_criticality = "c"                       # "A+","a+","A","a","B","b","C","c","Z","z","Tier 0","Tier0","T0,","tier 0","tier0","t0","Tier 1","Tier1","T1","tier 1","tier1","t1","N/A","NA"
     app_owner_group      = "BIGID_OPS_SUPPORT_GROUP" # Should be checked on CMDB on CI Application Owner Group

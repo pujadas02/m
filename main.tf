1c07755fb4091c1a98e6d164bb4cacc2e4be387d
pujadas02 <pd4759150@gmail.com>
2025-05-21 19:48:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,25 @@ resource "azurerm_resource_group" "primary" {
   name     = "hi"
   location = "Eastuss"
 }
+resource "azurerm_monitor_diagnostic_setting" "logic20" {
+  name                       = "OperationLogs"
+  target_resource_id = "j"
+  enabled_log {
+    category = "WorkflowRuntime"
+  }
+  metric {
+    category = "AllMetrics"
+  }
+}
 
+
+resource "azurerm_dns_zone" "example-public" {
+  name                = "mydomain.com"
+  resource_group_name = "hi"
+  tags = {
+    app = "T1"
+  }
+}
 resource "azurerm_resource_group" "rg_01" {
   name                = "mydomain.com"
   tags     = merge(local.tags.common_tags, local.tags.cvlt_backup.non_iaas)

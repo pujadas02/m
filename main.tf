1429b65a78ebc90c2d6958fb37d2b1c89512566d
pujadas02 <pd4759150@gmail.com>
2025-05-26 14:36:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,9 +6,10 @@ resource "azurerm_resource_group" "primary" {
   name     = "hi"
   location = "Eastuss"
 }
+
 resource "azurerm_resource_group" "rg_01" {
   name                = "mydomain.com"
-  tags     = merge(var.tags.cvlt_backup.non_iaas, local.tags.cvlt_backup.non_iaas, local.tags.common_tags)
+  tags     = merge(local.tags.cvlt_backup.non_iaas, local.tags.common_tags)
   location = "k"
 }
   

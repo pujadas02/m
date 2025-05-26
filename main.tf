7397169129bd8a30d52ae8930913466e3ad42505
pujadas02 <pd4759150@gmail.com>
2025-05-26 15:08:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,19 +9,7 @@ resource "azurerm_resource_group" "primary" {
 
 resource "azurerm_resource_group" "rg_01" {
   name                = "mydomain.com"
-  tags = {
-  app                  = "Mars Integration Services"
-  ppm_id_owner         = "Chen, Bruce"
-  ppm_io_cc            = "11631976"
-  environment          = "dev" # Allowed values are: 'production', 'legacy prod', 'disaster recovery', 'qa', 'QA', 'dev', 'Dev', 'test', 'Test', 'sandbox', 'Sandbox', 'N/A', 'NA'
-  data_classification  = "Confidential"
-  business_criticality = "A"
-  app_owner_group      = "OPS-APPHOSTING-BAI-SOLACE"
-  expert_centre        = "OPS-APPHOSTING-BAI-SOLACE"
-  cvlt_backup          = "cvlt_no_backup"
-  snapshotlifetime     = "0"
-  datadog              = "monitored"
-}
+  tags     = merge(local.tags.cvlt_backup.non_iaas, local.tags.common_tags)
   location = "k"
 }
   

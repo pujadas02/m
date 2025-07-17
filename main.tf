e2946bcbb959f6f678496bf4d0dda01624b93714
pujadas02 <pd4759150@gmail.com>
2025-07-17 18:52:21 +0530

    PASSED test for peering

--- main.tf
+++ main.tf
@@ -1,10 +1,9 @@
-# VPC Network 1 (This is the source network)
 resource "google_compute_network" "example_network" {
   name                    = "example-network"
   auto_create_subnetworks  = true
 }
 
-# Subnetwork 1 for the example network
+
 resource "google_compute_subnetwork" "example_subnet" {
   name          = "example-subnet"
   network       = google_compute_network.example_network.name
@@ -12,13 +11,13 @@ resource "google_compute_subnetwork" "example_subnet" {
   ip_cidr_range = "10.0.0.0/24"
 }
 
-# VPC Network 2 (This is the peer network)
+
 resource "google_compute_network" "peer_network" {
   name                    = "peer-network"
   auto_create_subnetworks  = true
 }
 
-# Subnetwork 2 for the peer network
+
 resource "google_compute_subnetwork" "peer_subnet" {
   name          = "peer-subnet"
   network       = google_compute_network.peer_network.name
@@ -26,11 +25,6 @@ resource "google_compute_subnetwork" "peer_subnet" {
   ip_cidr_range = "10.1.0.0/24"
 }
 
-# TEST CASE: VPC Peering Resource (This will trigger the Checkov policy)
-resource "google_compute_network_peering" "example_peering" {
-  name         = "example-vpc-peering"
-  network      = google_compute_network.example_network.name
-  peer_network = google_compute_network.peer_network.name
-  auto_create_routes = true
-}
+
+
 

e2db3b1795ff087f69e8baf12d376156473ee902
pujadas02 <pd4759150@gmail.com>
2025-07-24 00:12:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "google_compute_subnetwork" "subnetwork-ipv6" {
   ip_cidr_range = "10.0.0.0/22"
   region        = "us-west2"
   network       = google_compute_network.custom-test.id
-  stack_type    = "IPV4_IPV6"
+  stack_type    = "IPV4_ONLY"
 }
 
 

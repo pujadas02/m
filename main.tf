6ecaeb3d7fd55708150dd1051feafbf3e890ab44
pujadas02 <pd4759150@gmail.com>
2025-07-23 18:56:29 +0530

    PASSED : STACK TYPE IN subnet
    1. Subnetwork stack_type = IPV4_ONLY

--- main.tf
+++ main.tf
@@ -1,22 +1,7 @@
-resource "google_datastream_private_connection" "private_conn" {
-  name     = "private-connection"
-  location = "us-central1"
-}
-
-resource "google_datastream_connection_profile" "postgres_private" {
-  name                 = "postgres-private-profile"
-  location             = "us-central1"
-  connection_profile_id = "pg-private-profile"
-
-  postgresql_profile {
-    hostname = "10.0.0.5"
-    port     = 5432
-    username = "pg_user"
-    password = "pg_password"
-    database = "postgres_db"
-  }
-
-  private_connectivity {
-    private_connection = google_datastream_private_connection.private_conn.id
-  }
+resource "google_compute_subnetwork" "subnet_pass1" {
+  name       = "subnet-pass1"
+  region     = "us-central1"
+  network    = "default"
+  ip_cidr_range = "10.0.0.0/24"
+  stack_type = "IPV4_ONLY"
 }

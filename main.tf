fa7e5bc53d868336ec1b50363c8878ceb5ec607a
pujadas02 <pd4759150@gmail.com>
2025-07-23 22:40:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,19 +5,4 @@ resource "google_compute_subnetwork" "subnet_pass1" {
   ip_cidr_range = "10.0.0.0/24"
   stack_type = "IPV4_ONLY"
 }
-resource "google_compute_instance" "ipv4_only_instance" {
-  name         = "ipv4-instance"
-  machine_type = "e2-medium"
-  zone         = "us-central1-a"
 
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
-    }
-  }
-
-  network_interface {
-    network = "default"
-    stack_type = "IPV4_ONLY"
-  } 
-}

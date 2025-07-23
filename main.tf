504ff82822b14ae35e532ecec1f6258e57b6be30
pujadas02 <pd4759150@gmail.com>
2025-07-23 22:57:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,16 @@
+resource "google_compute_subnetwork" "subnetwork-ipv6" {
+  name          = "ipv6-test-subnetwork"
 
+  ip_cidr_range = "10.0.0.0/22"
+  region        = "us-west2"
+
+  stack_type       = "IPV4_IPV6"
+  ipv6_access_type = "EXTERNAL"
+
+  network       = google_compute_network.custom-test.id
+}
+
+resource "google_compute_network" "custom-test" {
+  name                    = "ipv6-test-network"
+  auto_create_subnetworks = false
+}

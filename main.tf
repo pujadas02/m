870c2f523d478fca3a021e6f974cfd4eac844093
pujadas02 <pd4759150@gmail.com>
2025-07-24 22:14:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,9 +2,5 @@ resource "google_compute_backend_service" "iap_enabled" {
   name     = "global-backend"
   protocol = "HTTP"
 
-  iap {
-    enabled              = false
-    oauth2_client_id     = "example-client-id"
-    oauth2_client_secret = "example-secret"
-  }
+  
 }

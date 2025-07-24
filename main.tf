9465592c3d44c73b8490d5d72a716882f0e04038
pujadas02 <pd4759150@gmail.com>
2025-07-24 18:01:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,11 @@ resource "google_bigquery_connection" "aws_connection" {
   provider      = google-beta
   connection_id = "my-aws-connection"
   friendly_name = "AWS Connection for BigQuery Omni"
+  aws {
+    cross_account_role_arn = "arn:aws:iam::123456789012:role/BigQueryOmniAccess"
+    s3_staging_dir         = "s3://my-bq-omni-staging-bucket"
+    region                 = "us-east-1"
+  }
 }
 
 resource "google_compute_subnetwork" "subnetwork-ipv6" {

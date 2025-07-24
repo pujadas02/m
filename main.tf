00801fec0914951d1599d123d7abad71e5b82e6e
pujadas02 <pd4759150@gmail.com>
2025-07-24 17:11:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,25 +1,21 @@
-resource "google_compute_subnetwork" "subnetwork-ipv6" {
-  name          = "ipv6-test-subnetwork"
-  region        = "us-west2"
-  stack_type    = "IPV4_ONLY"
-  ipv6_access_type = "EXTERNAL"
-}
-
-resource "google_compute_network" "vpc_fail" {
-  name                    = "vpc-ipv6"
-  auto_create_subnetworks = false
-  enable_ula_internal_ipv6 = false
-}
-
-resource "google_compute_instance" "default" {
-  name         = "my-instance"
-  machine_type = "n2-standard-2"
-  zone         = "us-central1-a"
-  network_interface {
-    network = "default"
-    stack_type = "IPV4_ONLY"
-    ipv6_access_config {
-    }
+resource "google_organization_iam_audit_config" "org_success" {
+  org_id  = "your-org-id"
+  service = "allServices"
+  audit_log_config {
+    log_type = "DATA_READ"
   }
 }
-
+resource "google_folder_iam_audit_config" "folder_success" {
+  folder_id = "your-folder-id"
+  service   = "allServices"
+  audit_log_config {
+    log_type = "ADMIN_READ"
+  }
+}
+resource "google_project_iam_audit_config" "project_success" {
+  project = "your-project-id"
+  service = "allServices"
+  audit_log_config {
+    log_type = "ADMIN_READ"
+  }
+}

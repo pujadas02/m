368d4b31d54f2a44eab1a48587dea01ab648fa9c
pujadas02 <pd4759150@gmail.com>
2025-07-24 22:47:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,3 +13,13 @@ resource "google_workbench_instance" "default" {
     }
   }
 }
+
+resource "google_vertex_ai_endpoint_with_model_garden_deployment" "deploy" {
+  publisher_model_name = "publishers/google/models/paligemma@paligemma-224-float32"
+  location             = "us-central1"
+  model_config {
+    accept_eula =  true
+  }
+}
+
+data "google_project" "project" {}

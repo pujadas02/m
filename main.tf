65f62598ed221de74cbd2054686d9de3c702f0fe
pujadas02 <pd4759150@gmail.com>
2025-07-24 18:14:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,27 +9,15 @@ resource "google_bigquery_connection" "aws_connection" {
   }
 }
 
-resource "google_compute_subnetwork" "subnetwork-ipv6" {
-  name          = "ipv6-test-subnetwork"
-  region        = "us-west2"
-  stack_type    = "IPV4_ONLY"
-  ipv6_access_type = "EXTERNAL"
-}
+resource "google_bigquery_connection" "connection" {
+  connection_id = local.bigquery_prefix
+  location      = var.bigquery_omni_region
+  friendly_name = local.bigquery_prefix
+  description   = "External connection to Azure"
 
-resource "google_compute_network" "vpc_fail" {
-  name                    = "vpc-ipv6"
-  auto_create_subnetworks = false
-  enable_ula_internal_ipv6 = false
-}
-
-resource "google_compute_instance" "default" {
-  name         = "my-instance"
-  machine_type = "n2-standard-2"
-  zone         = "us-central1-a"
-  network_interface {
-    network = "default"
-    stack_type = "IPV4_ONLY"
-    ipv6_access_config {
-    }
+  azure {
+    customer_tenant_id               = var.azure_tenant_id
+    federated_application_client_id = azuread_application.bigquery_connection.application_id
   }
 }
+

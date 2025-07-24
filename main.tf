74087b6d5491c620b5a33473adb023bc53dad9aa
pujadas02 <pd4759150@gmail.com>
2025-07-24 18:00:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,3 +4,27 @@ resource "google_bigquery_connection" "aws_connection" {
   friendly_name = "AWS Connection for BigQuery Omni"
 }
 
+resource "google_compute_subnetwork" "subnetwork-ipv6" {
+  name          = "ipv6-test-subnetwork"
+  region        = "us-west2"
+  stack_type    = "IPV4_ONLY"
+  ipv6_access_type = "EXTERNAL"
+}
+
+resource "google_compute_network" "vpc_fail" {
+  name                    = "vpc-ipv6"
+  auto_create_subnetworks = false
+  enable_ula_internal_ipv6 = false
+}
+
+resource "google_compute_instance" "default" {
+  name         = "my-instance"
+  machine_type = "n2-standard-2"
+  zone         = "us-central1-a"
+  network_interface {
+    network = "default"
+    stack_type = "IPV4_ONLY"
+    ipv6_access_config {
+    }
+  }
+}

35d66e679f8024fb48faaafcaefef1e04a7f7b2e
pujadas02 <pd4759150@gmail.com>
2025-07-24 14:08:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,5 @@
 resource "google_compute_subnetwork" "subnetwork-ipv6" {
   name          = "ipv6-test-subnetwork"
-  ip_cidr_range = "2600:1900::/64"
   region        = "us-west2"
   stack_type    = "IPV4_ONLY"
 }
@@ -8,7 +7,7 @@ resource "google_compute_subnetwork" "subnetwork-ipv6" {
 resource "google_compute_network" "vpc_fail" {
   name                    = "vpc-ipv6"
   auto_create_subnetworks = false
-  enable_ula_internal_ipv6 = true  
+  enable_ula_internal_ipv6 = false
 }
 
 resource "google_compute_instance" "default" {
@@ -18,9 +17,6 @@ resource "google_compute_instance" "default" {
   network_interface {
     network = "default"
     stack_type = "IPV4_ONLY"
-    alias_ip_range {
-      ip_cidr_range = "2600:1900::/64"             
-    }
   }
 }
 

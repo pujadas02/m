0c5f3bac6b2b2a74a22e2ac023eb36b1e5bc55f2
pujadas02 <pd4759150@gmail.com>
2025-07-24 20:30:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,41 +1,10 @@
-resource "google_compute_ssl_certificate" "self_managed" {
-  name        = "global-self-managed-cert"
-  private_key = file("private-key.pem")
-  certificate = file("certificate.pem")
-}
+resource "google_compute_backend_service" "iap_enabled" {
+  name     = "global-backend"
+  protocol = "HTTP"
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-resource "google_bigquery_connection" "aws_connection" {
-  provider      = google-beta
-  connection_id = "my-aws-connection"
-  friendly_name = "AWS Connection for BigQuery Omni"
-  aws {
-    cross_account_role_arn = "arn:aws:iam::123456789012:role/BigQueryOmniAccess"
-    s3_staging_dir         = "s3://my-bq-omni-staging-bucket"
-    region                 = "us-east-1"
+  iap {
+    enabled              = true
+    oauth2_client_id     = "example-client-id"
+    oauth2_client_secret = "example-secret"
   }
 }
-resource "google_bigquery_connection" "connection" {
-  connection_id = local.bigquery_prefix
-  location      = var.bigquery_omni_region
-  friendly_name = local.bigquery_prefix
-  description   = "External connection to Azure"
-
-  azure {
-    customer_tenant_id               = var.azure_tenant_id
-    federated_application_client_id = azuread_application.bigquery_connection.application_id
-  }
-}
-

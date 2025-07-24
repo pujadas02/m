d1ba3eb25b0a029e734bdde4f45a80d728ae7606
pujadas02 <pd4759150@gmail.com>
2025-07-24 23:46:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,9 +2,9 @@ resource "google_compute_instance" "secure_vm" {
   name         = "secure-vm"
   machine_type = "e2-medium"
   zone         = "us-central1-a"
-  metadata = {
-    serial-port-enable = "true"
-  }
+
+  enable_guest_attributes = false
+
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-11"

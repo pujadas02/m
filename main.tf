85c4e0ea39c6e14a9df4654f07a3920c644bda33
pujadas02 <pd4759150@gmail.com>
2025-07-25 19:38:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,12 @@
-resource "google_compute_instance" "secure_vm" {
-  name         = "secure-vm"
-  machine_type = "e2-medium"
-  zone         = "us-central1-a"
-  metadata = {
-    enable-guest-attributes = "false"
-  }
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
-    }
-  }
-
-  network_interface {
-    network = "default"
-    access_config {}
+resource "google_project_iam_policy" "example" {
+  project     = "my-project-id"
+  policy_data = data.google_iam_policy.admin.policy_data
+}
+data "google_iam_policy" "admin" {
+  binding {
+    role = "roles/iap.tunnelResourceAccessor"
+    members = [
+      "user:foo@example.com",
+    ]
   }
 }

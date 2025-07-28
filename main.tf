9a2d1c6699e0c7f39ecb19448754e85fea0518da
pujadas02 <pd4759150@gmail.com>
2025-07-28 12:25:22 +0530

    will pass as serial-port-logging-enable does not exists

--- main.tf
+++ main.tf
@@ -3,5 +3,5 @@ resource "google_compute_instance" "default" {
   machine_type = "n2-standard-2"
   zone         = "us-central1-a"
   metadata = {
-    serial-port-logging-enable = "false"
+    
   }

aea06e8b5b450396087db78e7e486f28fd5485ca
pujadas02 <pd4759150@gmail.com>
2025-07-28 11:45:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,60 +1,15 @@
-resource "google_project_iam_member" "example_member" {
-  project = "my-project-id"
-  role   = "roles/iap.tunnelResourceAccessor"
-  member  = "user:example-user@example.com"
-}
+resource "google_workbench_instance" "default" {
+  name     = "workbench-instance-example"
+  location = "us-central1-a"
 
-resource "google_project_iam_binding" "example_binding" {
-  project = "my-project-id"
- 
-  members = [
-    "user:example-user@example.com",
-    "serviceAccount:example-sa@my-project-id.iam.gserviceaccount.com",
-  ]
-}
-
-resource "google_project_iam_policy" "example_policy" {
-  project     = "my-project-id"
-  policy_data = data.google_iam_policy.admin.policy_data
-}
-
-data "google_iam_policy" "admin" {
-  binding {
-    
-    members = [
-      "user:example-user@example.com",
-    ]
+  gce_setup {
+    machine_type = "n1-standard-1"
+    vm_image {
+      project = "cloud-notebooks-managed"
+      family  = "workbench-instances"
+    }
+    metadata = {
+     "notebook-disable-terminal" = "true"
+    }
   }
 }
-
-
-
-resource "google_folder_iam_member" "example_folder_member" {
-  folder = "folders/123456789"
-  
-  member = "user:example-user@example.com"
-}
-
-resource "google_folder_iam_binding" "example_folder_binding" {
-  folder = "folders/123456789"
-  
-  members = [
-    "user:example-user@example.com",
-  ]
-}
-
-
-resource "google_organization_iam_member" "example_org_member" {
-  org_id = "123456789012"
-  
-  member = "user:example-user@example.com"
-}
-
-resource "google_organization_iam_binding" "example_org_binding" {
-  org_id = "123456789012"
-  
-  members = [
-    "user:example-user@example.com",
-  ]
-}
-

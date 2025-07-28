bbc0edbb791abb9b426daf06bbd427e56cb2ae41
pujadas02 <pd4759150@gmail.com>
2025-07-28 13:17:56 +0530

    it will pass

--- main.tf
+++ main.tf
@@ -11,15 +11,4 @@ resource "google_compute_network" "vpc_fail" {
   enable_ula_internal_ipv6 = false
 }
 
-resource "google_compute_instance" "default" {
-  name         = "my-instance"
-  machine_type = "n2-standard-2"
-  zone         = "us-central1-a"
-  network_interface {
-    network = "default"
-    stack_type = "IPV4_ONLY"
-    ipv6_access_config {
-    }
-  }
-}
 

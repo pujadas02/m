70fbb85b58e8fcd3729abab195977b5d2e0a36f8
pujadas02 <pd4759150@gmail.com>
2025-07-28 14:49:27 +0530

    will pass

--- main.tf
+++ main.tf
@@ -2,13 +2,13 @@ resource "google_compute_subnetwork" "subnetwork-ipv6" {
   name          = "ipv6-test-subnetwork"
   region        = "us-west2"
   stack_type    = "IPV4_ONLY"
-  ipv6_access_type = "INTERNAL"
+  ipv6_access_type = "external"
 }
 
 resource "google_compute_network" "vpc_fail" {
   name                    = "vpc-ipv6"
   auto_create_subnetworks = false
-  
+  enable_ula_internal_ipv6 = false
 }
 
 

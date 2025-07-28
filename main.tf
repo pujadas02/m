d92adb2a2422f44c486168036d699e1ff5760254
pujadas02 <pd4759150@gmail.com>
2025-07-28 15:58:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,6 @@
 resource "google_project_iam_audit_config" "iap-audit-logging" {
   service = "iap.googleapis.com"
-  audit_log_config {
-    log_type = "DATA_READ"
-  }
-  audit_log_config {
-    log_type = "DATA_WRITE"
-  }
-  audit_log_config {
-    log_type = "ADMIN_READ"
-  }
-  audit_log_config {
-    log_type = "DAT_WRITE"
-  }
+
 
 }
 

8c0e8c31fec1915db75b022c1bc2adea20acf7fc
pujadas02 <pd4759150@gmail.com>
2025-07-28 15:20:54 +0530

    will fail

--- main.tf
+++ main.tf
@@ -1,12 +1,6 @@
 resource "google_project_iam_audit_config" "iap-audit-logging" {
   service = "iap.googleapis.com"
-  audit_log_config {
-    log_type = "DATA_READ"
-  }
-
-  audit_log_config {
-    log_type = "ADMIN_READ"
-  }
+  
 
 }
 

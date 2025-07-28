fd4dcf0b9dc5433711380369e5d19f2a301c9d30
pujadas02 <pd4759150@gmail.com>
2025-07-28 18:55:21 +0530

    FAIL as resource is presnt

--- main.tf
+++ main.tf
@@ -3,6 +3,10 @@ resource "google_service_account" "myaccount" {
   display_name = "My Service Account"
 }
 
+resource "google_service_account_key" "mykey" {
+  service_account_id = google_service_account.myaccount.name
+  public_key_type    = "TYPE_X509_PEM_FILE"
+}
 
 
 

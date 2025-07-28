b04b3f62bf7fa405306239a412db4cdc4c9b74bb
pujadas02 <pd4759150@gmail.com>
2025-07-28 15:19:14 +0530

    should pass

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 resource "google_project_iam_audit_config" "iap-audit-logging" {
   service = "iap.googleapis.com"
   audit_log_config {
-    log_type = "ADMIN_READ"
+    log_type = "DATA_READ"
   }
   audit_log_config {
     log_type = "DATA_WRITE"
   }
   audit_log_config {
-    log_type = "DATA_READ"
+    log_type = "ADMIN_READ"
   }
 }

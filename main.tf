d2ebb394a279dde85e0f625e5c812d1a0e622107
pujadas02 <pd4759150@gmail.com>
2025-07-28 14:56:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,20 +1,13 @@
-resource "google_compute_subnetwork" "subnetwork-ipv6" {
-  name          = "ipv6-test-subnetwork"
-  region        = "us-west2"
-  stack_type    = "IPV4_ONLY"
-  ipv6_access_type = "external"
-}
-
-resource "google_compute_network" "vpc_fail" {
-  name                    = "vpc-ipv6"
-  auto_create_subnetworks = false
-  enable_ula_internal_ipv6 = false
-}
-
-
-resource "google_compute_network_attachment" "default" {
-    name = "basic-network-attachment"
-    region = "us-central1"
-    description = "basic network attachment description"
-    connection_preference = "ACCEPT_MANUAL"
+resource "google_project_iam_audit_config" "project" {
+  project = "your-project-id"
+  service = "allServices"
+  audit_log_config {
+    log_type = "ADMIN_READ"
+  }
+  audit_log_config {
+    log_type = "DATA_READ"
+    exempted_members = [
+      "user:joebloggs@hashicorp.com",
+    ]
+  }
 }

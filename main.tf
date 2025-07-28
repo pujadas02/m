1d32c2ac92beb78fc51ec376a7dd879768d44b2d
pujadas02 <pd4759150@gmail.com>
2025-07-28 18:54:59 +0530

    will pass as the resource is absent

--- main.tf
+++ main.tf
@@ -3,10 +3,6 @@ resource "google_service_account" "myaccount" {
   display_name = "My Service Account"
 }
 
-resource "google_service_account_key" "mykey" {
-  service_account_id = google_service_account.myaccount.name
-  public_key_type    = "TYPE_X509_PEM_FILE"
-}
 
 
 

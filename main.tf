3b9752e10c0620e86fa0fecc7631f43b07669a11
pujadas02 <pd4759150@gmail.com>
2025-07-30 16:46:13 +0530

    must pass as the whole resource doesnot exists

--- main.tf
+++ main.tf
@@ -1,16 +1,3 @@
-resource "google_sql_database_instance" "main" {
-  name             = "psc-enabled-main-instance"
-  database_version = "MYSQL_8_0"
-  settings {
-    tier    = "db-f1-micro"
-
-    backup_configuration {
-      enabled = true
-      binary_log_enabled = true
-    }
-    availability_type = "REGIONAL"
-  }
-}
 
 resource "google_compute_network_attachment" "default" {
     name = "basic-network-attachment"

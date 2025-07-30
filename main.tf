86e5f398d5e377db2b1a9886bad23d233684eb28
pujadas02 <pd4759150@gmail.com>
2025-07-30 16:41:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,62 +1,22 @@
-resource "google_sql_database_instance" "example" {
-  name             = "example-instance"
-  database_version = "POSTGRES_14"
-  region           = "us-central1"
-
+resource "google_sql_database_instance" "main" {
+  name             = "psc-enabled-main-instance"
+  database_version = "MYSQL_8_0"
   settings {
-    tier = "db-f1-micro"
-
+    tier    = "db-f1-micro"
     ip_configuration {
-      ipv4_enabled    = true
-      authorized_networks = [
-        {
-          name  = "office-network"
-          value = "203.0.113.0/24"
-        },
-        {
-          name  = "home-network"
-          value = "198.51.100.0/24"
-        }
-      ]
+      psc_config {
+        psc_enabled = true
+        allowed_consumer_projects = ["allowed-consumer-project-name"]
+      }
+      ipv4_enabled = false
     }
+    backup_configuration {
+      enabled = true
+      binary_log_enabled = true
+    }
+    availability_type = "REGIONAL"
   }
 }
-
-
-resource "google_sql_database_instance" "postgres" {
-  name             = "postgres-instance-${random_id.db_name_suffix.hex}"
-  database_version = "POSTGRES_15"
-
-  settings {
-    tier = "db-f1-micro"
-
-    ip_configuration {
-
-      dynamic "authorized_networks" {
-        for_each = google_compute_instance.apps
-        iterator = apps
-
-        content {
-          name  = apps.value.name
-          value = apps.value.network_interface.0.access_config.0.nat_ip
-        }
-      }
-      dynamic "authorized_networks" {
-        for_each = local.onprem
-        iterator = onprem
-
-        content {
-          name  = "onprem-${onprem.key}"
-          value = onprem.value
-        }
-      }
-      
-
-    }
-  }
-}
-
-
 
 resource "google_compute_network_attachment" "default" {
     name = "basic-network-attachment"

f595e7d1a4b8a2fd00cf9cacce3e4b76b48e7f8a
pujadas02 <pd4759150@gmail.com>
2025-07-31 17:47:32 +0530

    pass

--- main.tf
+++ main.tf
@@ -2,14 +2,12 @@ resource "google_compute_instance" "secure_vm" {
   name         = "secure-vm"
   machine_type = "e2-medium"
   zone         = "us-central1-a"
-  metadata = {
-    enable-guest-attributes = "false"
-  }
+
 }
 
 
 resource "google_compute_project_metadata_item" "default" {
-  key   = "enble-guest-attributes"
+  key   = "enable-guest-attributes"
   value = "true"
 }
 

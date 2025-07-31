2d5f7ca6bc00c533c5331c9c611411e2996e3f9a
pujadas02 <pd4759150@gmail.com>
2025-07-31 22:03:46 +0530

    should pass

--- main.tf
+++ main.tf
@@ -1,8 +1,19 @@
-resource "google_compute_instance" "default" {
-  name         = "my-instance"
-  machine_type = "n2-standard-2"
+resource "google_compute_instance" "secure_vm" {
+  name         = "secure-vm"
+  machine_type = "e2-medium"
   zone         = "us-central1-a"
   metadata = {
-    serial-port-logging-enable = "true"
+    serial-port-logging-enable = "false"
+  }
+}
+
+resource "google_compute_project_metadata_item" "default" {
+  key   = "serial-port-logging-enable"
+  value = "false"
+}
+
+resource "google_compute_project_metadata" "default" {
+  metadata = {
+    serial-port-logging-enable = "false"
   }
 }

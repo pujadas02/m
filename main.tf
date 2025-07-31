f7c372451053467c3f5ec0d3ee0fa47320f53d5e
pujadas02 <pd4759150@gmail.com>
2025-07-31 17:40:08 +0530

    should fail

--- main.tf
+++ main.tf
@@ -9,8 +9,13 @@ resource "google_compute_instance" "secure_vm" {
 
 resource "google_compute_project_metadata_item" "default" {
   key   = "enable-guest-attributes"
-  value = "false"
+  value = "true"
 }
 
+
+resource "google_compute_project_metadata" "default" {
+  metadata = {
+    
+  }
 
 }

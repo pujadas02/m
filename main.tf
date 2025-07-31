07a7f792a1d6dffbd68bcbf92d649f9b4eab59f4
pujadas02 <pd4759150@gmail.com>
2025-07-31 17:47:00 +0530

    should pass

--- main.tf
+++ main.tf
@@ -3,19 +3,18 @@ resource "google_compute_instance" "secure_vm" {
   machine_type = "e2-medium"
   zone         = "us-central1-a"
   metadata = {
-    enable-guest-attributes = "true"
+    enable-guest-attributes = "false"
   }
 }
 
 
 resource "google_compute_project_metadata_item" "default" {
-  key   = "enable-guest-attributes"
-  value = "true"
+
 }
 
 resource "google_compute_project_metadata" "default" {
   metadata = {
-    enable-guest-attributes = "true"
+    enable-guest-attributes = "false"
   }
 }
 

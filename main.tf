5ece1f2ac6ef02e5eef11f4d940dcc96ee104a74
pujadas02 <pd4759150@gmail.com>
2025-07-31 17:49:32 +0530

    pass

--- main.tf
+++ main.tf
@@ -1,9 +1,4 @@
-resource "google_compute_instance" "secure_vm" {
-  name         = "secure-vm"
-  machine_type = "e2-medium"
-  zone         = "us-central1-a"
 
-}
 
 
 resource "google_compute_project_metadata_item" "default" {

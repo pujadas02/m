4add8428513944b21ab200e8f9171fe679b79b4f
pujadas02 <pd4759150@gmail.com>
2025-07-31 22:22:04 +0530

    fail

--- main.tf
+++ main.tf
@@ -14,6 +14,6 @@ resource "google_compute_project_metadata_item" "default" {
 
 resource "google_compute_project_metadata" "default" {
   metadata = {
-    serial-port-logging-enable = "true"
+    serial-port-logging-enable = "false"
   }
 }

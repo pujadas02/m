9c961ffd2c4e45fdfda30570136f37d2cb603d96
pujadas02 <pd4759150@gmail.com>
2025-07-31 22:24:32 +0530

    pass

--- main.tf
+++ main.tf
@@ -2,16 +2,9 @@ resource "google_compute_instance" "secure_vm" {
   name         = "secure-vm"
   machine_type = "e2-medium"
   zone         = "us-central1-a"
-
-}
-
-resource "google_compute_project_metadata_item" "default" {
-  key   = "serial-port-logging-enable"
-  value = "false"
-}
-
-resource "google_compute_project_metadata" "default" {
   metadata = {
     serial-port-logging-enable = "false"
   }
 }
+
+

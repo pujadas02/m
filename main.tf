c5f050699c6df03a916d1060b5d416ee71fbd012
pujadas02 <pd4759150@gmail.com>
2025-07-31 22:21:52 +0530

    fail

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ resource "google_compute_instance" "secure_vm" {
 
 resource "google_compute_project_metadata_item" "default" {
   key   = "serial-port-logging-enable"
-  value = "false"
+  value = "true"
 }
 
 resource "google_compute_project_metadata" "default" {

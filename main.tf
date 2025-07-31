fe584341bcabcca72cbcea4097a142bbb55086bb
pujadas02 <pd4759150@gmail.com>
2025-07-31 17:49:41 +0530

    pass

--- main.tf
+++ main.tf
@@ -1,14 +1,1 @@
 
-
-
-resource "google_compute_project_metadata_item" "default" {
-  key   = "enable-guest-attributes"
-  value = "false"
-}
-
-resource "google_compute_project_metadata" "default" {
-  metadata = {
-    enable-guest-attributes = "false"
-  }
-}
-

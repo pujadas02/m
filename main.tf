3daee0f5a86574c1610d51af595fdc92cc1c5c3f
pujadas02 <pd4759150@gmail.com>
2025-07-31 17:50:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,3 +10,13 @@ resource "google_compute_instance" "secure_vm" {
 
 
 
+resource "google_compute_project_metadata_item" "default" {
+  key   = "enable-guest-attributes"
+  value = "false"
+}
+
+resource "google_compute_project_metadata" "default" {
+  metadata = {
+    enable-guest-attributes = "false"
+  }
+}

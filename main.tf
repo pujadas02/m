3789bf04357741209ab42e901fe81f40d867e3d1
pujadas02 <pd4759150@gmail.com>
2025-07-31 14:53:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,32 +1,34 @@
-resource "google_compute_project_metadata" "default" {
-  metadata = {
-    enable-oslogin = "TRUE"
+data "google_project" "project" {}
+
+resource "google_cloudbuild_trigger" "service-account-trigger" {
+  trigger_template {
+    branch_name = "main"
+    repo_name   = "my-repo"
   }
+
+  service_account = google_service_account.cloudbuild_service_account.id
+  filename        = "cloudbuild.yaml"
+  depends_on = [
+    google_project_iam_member.act_as,
+    google_project_iam_member.logs_writer
+  ]
 }
 
-resource "google_compute_instance" "oslogin_instance" {
-  name         = "oslogin-instance-name"
-  machine_type = "f1-micro"
-  zone         = "us-central1-c"
-  metadata = {
-
-  }
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
-    }
-  }
-  network_interface {
-    # A default network is created for all GCP projects
-    network = "default"
-    access_config {
-    }
-  }
+resource "google_service_account" "cloudbuild_service_account" {
+  account_id = "cloud-sa"
 }
 
+resource "google_project_iam_member" "act_as" {
+  project = data.google_project.project.project_id
+  role    = "roles/iam.serviceAccountUser"
+  member  = "serviceAccount:${google_service_account.cloudbuild_service_account.email}"
+}
 
-
-
+resource "google_project_iam_member" "logs_writer" {
+  project = data.google_project.project.project_id
+  role    = "roles/logging.logWriter"
+  member  = "serviceAccount:${google_service_account.cloudbuild_service_account.email}"
+}
 
 
 

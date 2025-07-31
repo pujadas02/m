bafa199724f91673740c70985cdd3b45817d04df
pujadas02 <pd4759150@gmail.com>
2025-07-31 17:43:01 +0530

    should pass

--- main.tf
+++ main.tf
@@ -9,4 +9,15 @@ resource "google_compute_instance" "secure_vm" {
 
 
 
+resource "google_compute_project_metadata_item" "default" {
+  key   = "attributes"
+  value = "false"
 }
+
+resource "google_compute_project_metadata" "default" {
+  metadata = {
+    
+  }
+
+
+

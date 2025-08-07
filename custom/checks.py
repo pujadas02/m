6fe49d5ab5901f65c978c85ef37553d31e12511b
pujadas02 <pd4759150@gmail.com>
2025-08-07 21:06:05 +0530

    Update checks.py

--- custom/checks.py
+++ custom/checks.py
@@ -1,6 +1,5 @@
 from checkov.terraform.checks.resource.base_resource_check import BaseResourceValueCheck
 from checkov.common.models.enums import CheckCategories, CheckResult
-
 class AllowedExternalIPv4ForVMCheck(BaseResourceValueCheck):
     def __init__(self):
         super().__init__(

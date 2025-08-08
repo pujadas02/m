37c23ce955a9bd3c98ea998300c8664f6373d8d1
pujadas02 <pd4759150@gmail.com>
2025-08-08 14:09:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,6 +13,12 @@ resource "google_compute_instance" "allowed_vm" {
   can_ip_forward = true
 }
 
+resource "google_compute_instance" "allowed_vm" {
+  name         = "allowed-vm"
+  machine_type = "e2-medium"
+  zone         = "us-central1-a"
+  can_ip_forward = false
+}
 
 resource "google_compute_instance" "vm_static" {
   name         = "disallowed-vm-1"
